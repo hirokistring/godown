07ab9fc138e9a3dacf2fee73144e8d376149e612
Hiroki Saito <105449164+hirokistring@users.noreply.github.com>
2022-06-04 01:44:35 +0900

    Added example image

---
+++ docs/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+const WHO = "godown"
+
+func main() {
+	fmt.Printf("Hello,%s!\n", WHO)
+}
+
+// This file is generated from "hello.go.md" by godown.
+// https://github.com/hirokistring/godown
---
+++ docs/hello.go.md
@@ -0,0 +1,14 @@
+# hello.go
+
+```go
+package main
+import "fmt"
+
+const WHO = "godown"
+```
+
+## func `main`
+
+```go
+fmt.Printf("Hello, %s!\n",WHO)
+```
